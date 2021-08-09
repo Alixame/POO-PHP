e6347994f41089d633f9a2691c2045b33396e1a1
Lucas Alixame <lucasali2003@gmail.com>
2021-08-09 08:55:16 -0300

    Adicionando Material

--- PHP-OB/6-HerancaDeClasses.php
+++ PHP-OB/6-HerancaDeClasses.php
@@ -38,11 +38,29 @@ class CPF extends Documento { // <- Criando classe 'CPF' que herda atributos da 
 
 }
 
+class RG extends Documento { // <- Criando classe 'CPF' que herda atributos da classe 'Documento'
+
+    // OBS: Note-se que não declaramos nenhuma variavel nessa classe, podemos usar direto da classe herdada
+
+    // Criando função para validar cpf setando um retorno padrão (bool = True ou False)
+    public function validar():bool {
+
+        //Validação do CPF
+        $numeroCPF = $this->getNumero();
+
+        //Retorna se a validação deu certo
+        return true;
+
+    }
+
+}
+
+// EXEMPLO CPF
 //Instanciando um novo Objeto da Classe CPF que herda atributos da classe Documento
 $cpf = new CPF();
 
 //Atribui o 'Numero' do Objeto "cpf"
-$cpf->setNumero('46153637885');
+$cpf->setNumero('554.912.868-34');
 
 //Mostrando na tela o retorno da validação
 var_dump($cpf->validar());
@@ -52,4 +70,22 @@ echo "<br>";
 //Mostrando na tela o valor do atributo 'numero' do objeto 'cpf'
 var_dump($cpf->getNumero());
 
+///////////////////////////////////////////////////////////////
+echo '<hr>';
+///////////////////////////////////////////////////////////////
+
+// EXEMPLO RG
+//Instanciando um novo Objeto da Classe RG que herda atributos da classe Documento
+$rg = new RG();
+
+//Atribui o 'Numero' do Objeto "rg"
+$rg->setNumero('28.717.830-A');
+
+//Mostrando na tela o retorno da validação
+var_dump($rg->validar());
+
+echo "<br>";
+
+//Mostrando na tela o valor do atributo 'numero' do objeto 'rg'
+var_dump($rg->getNumero());
 
